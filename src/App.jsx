d2fecf2b516e9750194fd67dd27d7f81758ae05d
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-23 17:39:47 +0600

    render with react-window fixed-size-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import List from './List'
+//import List from './List'
+import faker from 'faker'
+import {FixedSizeList} from 'react-window'
+
+const bigList = [...Array(6000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar()
+}))
 
 const tahoe_peaks = [
   {name: "Freel Peak", elevation: 10891},
@@ -9,17 +17,32 @@ const tahoe_peaks = [
 ]
 
 function App() {
+  const renderRow = ({index, style}) => (
+    <div style={{...style, display: "flex"}}>
+      <img src={bigList[index].avatar} alt={bigList[index].name} />
+      <p>{bigList[index].name} - {bigList[index].email}</p>
+    </div>
+  )
+
   return (
-    <List 
-      data={tahoe_peaks} 
-      renderEmpty={ <p>This list is empty</p> } 
-      renderItem={item => (
-        <>
-          {item.name} - {item.elevation.toLocaleString()}ft
-        </>
-      )}
-    />
-  );
+    <FixedSizeList 
+      height={window.innerHeight}
+      width={window.innerHeight}
+      itemCount={bigList.length}
+      itemSize={100}
+    >{renderRow}</FixedSizeList>
+
+    // <List 
+    //   data={bigList} 
+    //   renderEmpty={ <p>This list is empty</p> } 
+    //   renderItem={item => (
+    //     <div style={{ display: "flex" }}>
+    //       <img src={item.avatar} alt="item.name"/>
+    //       <p>{item.name} - {item.email}</p>
+    //     </div>
+    //   )}
+    // />
+  )
 }
 
 export default App;
